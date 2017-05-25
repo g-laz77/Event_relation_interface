91c5582a0113bdff30574d8c49f2b2a50316c567
g-laz77 <suhanprabhu@gmail.com>
2017-05-25 19:09:06 +0530

    finished adding drop down. Need to just add some categories.

--- tut.py
+++ tut.py
@@ -13,7 +13,7 @@ phonelist = [['Chris', 'Meyers', '241-343-4349'],
 def make_window():
     global select1, select2
     win = Tk()
-    frame3 = Frame(win)       # select of names
+    frame3 = Frame(win)       # event display
     frame3.pack()
     Label(frame3, text="ID").grid(row=0, column=0, sticky=W)
     scroll1 = Scrollbar(frame3, orient=VERTICAL)
@@ -21,12 +21,57 @@ def make_window():
     select1 = Listbox(frame3, yscrollcommand=scroll1.set, height=6)
     select2 = Listbox(frame3, yscrollcommand=scroll2.set, height=6)
     scroll1.config(command=select1.yview)
-    scroll1.pack(side=RIGHT, fill=Y)
+    scroll1.grid(row = 1, column = 0)
     scroll2.config(command=select2.yview)
-    scroll2.pack(side=RIGHT, fill=Y)
+    scroll2.grid(row = 1, column = 1)
     Label(frame3, text="Event").grid(row=0, column=1, sticky=W)
-    select1.pack(side=LEFT, fill=BOTH, expand=1)
-    select2.pack(side=RIGHT, fill=BOTH, expand=1)
+    select1.grid(row = 1, column = 0)
+    select2.grid(row = 1, column = 1)
+
+    #input
+    frame2 = Frame(win)
+    frame2.pack()
+
+    
+
+    Label(frame2, text="Source-Event").grid(row=2, column=0)
+    sour_evt = StringVar()
+    evs = Entry(frame2, textvariable=sour_evt)
+    evs.grid(row=3, column=0, sticky=W)
+
+    Label(frame2, text="Destination-Event").grid(row=2, column=1, sticky=W)
+    dest_evt = StringVar()
+    evd = Entry(frame2, textvariable=dest_evt)
+    evd.grid(row=3, column=1, sticky=W)
+
+    Label(frame2, text="Source-Timex").grid(row=2, column=2, sticky=W)
+    sour_tim = StringVar()
+    tvs = Entry(frame2, textvariable=sour_tim)
+    tvs.grid(row=3, column=2, sticky=W)
+
+    Label(frame2, text="Destination-Timex").grid(row=2, column=3, sticky=W)
+    dest_tim = StringVar()
+    tvd = Entry(frame2, textvariable=dest_tim)
+    tvd.grid(row=3, column=3, sticky=W)
+
+    Label(frame2, text="Event-Relation").grid(row=2, column=4, sticky=W)
+    variable = StringVar()
+    variable.set("SUBEVENT") # default value
+    w = OptionMenu(frame2, variable,"SUBEVENT", "PURPOSE", "REASON", "ENABLEMENT", "PRECEDENCE", "RELATED")
+    w.grid(row=3, column =4, sticky=W)
+
+    Label(frame2, text="Timex-Relation").grid(row=2, column=5, sticky=W)    
+    var = StringVar()
+    var.set("BEFORE") # default value
+    w = OptionMenu(frame2, var,"BEFORE", "AFTER" , "INCLUDES" , "IS_INCLUDED" ,"DURING" , "DURING_INV" , "SIMULTANEOUS" , "IAFTER" , "IBEFORE" ,"IDENTITY" , "BEGINS" , "ENDS" , "BEGUN_BY" , "ENDED_BY")
+    w.grid(row=3, column = 5, sticky=W)
+
+    Label(frame2, text="Link-Type").grid(row=2, column=6, sticky=W)    
+    var2 = StringVar()
+    var2.set("LINK") # default value
+    w = OptionMenu(frame2, var2,"LINK","TLINK")
+    w.grid(row=3, column = 6, sticky=W)
+
     return win
 
 
@@ -39,7 +84,6 @@ def set_select():
         select1.insert(END, "{0}".format(lname))
         select2.insert(END, "{0}".format(fname))
         
-
 
 win = make_window()
 set_select()
