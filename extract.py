78d20e9f331af11515e87019ae808d859b47492a
g-laz77 <suhanprabhu@gmail.com>
2017-05-27 12:33:24 +0530

    fixed commit

--- extract.py
+++ extract.py
@@ -20,34 +20,22 @@ def extractor(filename):
             final_dict[child.text] = child.attrib
             # print(final_dict)
             # print(child.attrib)
-<<<<<<< HEAD
-            tree.write('output.xml')
-=======
             tree.write('output2.xml', encoding="UTF-8", xml_declaration=True)
->>>>>>> f5296e8d24f8f782696d88eb05ee665a82a853f6
         elif child.tag == 'EVENT':
             child.attrib.pop('tense')
             child.attrib.pop('aspect')
             final_dict[child.text] = child.attrib
             # print(final_dict)
             # print(child.attrib)
-<<<<<<< HEAD
-            tree.write('output.xml')
-=======
             tree.write('output2.xml', encoding="UTF-8", xml_declaration=True)
->>>>>>> f5296e8d24f8f782696d88eb05ee665a82a853f6
 
     json = simplejson.dumps(final_dict, ensure_ascii=False)
 
     # f = open('output.txt','w+')
     # f.write(json)
     return json
-<<<<<<< HEAD
     l = open(filename, "w")
 
 extractor("data.xml")
-=======
 
 
-extractor('data.xml')
->>>>>>> f5296e8d24f8f782696d88eb05ee665a82a853f6
